ecc7615adbb324cb530367555eedcd400fab477f
Carlos Duran <krlosduran117@gmail.com>
2023-10-10 19:54:21 -0500

    New data added and Footer icon

---
+++ src/components/Footer.jsx
@@ -0,0 +1,14 @@
+import { FooterIcon } from "../icons"
+import './Footer.css'
+
+
+const Footer = () => {
+  return (
+    <footer>
+      <button className='footer'>
+        <FooterIcon />
+      </button>
+    </footer>
+  )
+}
+export default Footer
---
+++ src/data.js
@@ -0,0 +1,27 @@
+export const task = [
+  {
+    id: 0,
+    title: `Make dinner`,
+    description: `Prepare the dinner for my Sister's birthday`,
+    subtask: [],
+    dueDate: '23-12-2023',
+    tag: '',
+    project: '',
+  },
+  {
+    id: 1,
+    title: `New PC`,
+    description: `Buy a new PC for my birthday`,
+    subtask: [],
+    dueDate: '14-02-2024',
+    tag: '',
+    project: '',
+  }
+];
+
+export const user = [
+  { id: 1, name: 'Michael' },
+  { id: 2, name: 'Dwight' },
+  { id: 3, name: 'Jim' },
+  { id: 4, name: 'Pam' },
+];
