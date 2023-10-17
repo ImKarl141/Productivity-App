b49811aac852ad873f03eb09c546de9f3078d870
Carlos Duran <krlosduran117@gmail.com>
2023-10-17 16:56:11 -0500

    Base layout of Note component

--- src/components/Notes.jsx
+++ src/components/Notes.jsx
@@ -1,6 +1,28 @@
+import './Notes.css'
+import { AddNoteIcon } from '../icons'
+
 const NotesMenu = () => {
   return (
-    <div>Hello from Notes</div>
+    <section className="notes-container">
+      <div className="notes-overall">
+        <div className='note-item-add'>
+          <AddNoteIcon />
+        </div>
+        <div className='note-item'>
+          <div className='note-item-container'>
+            <div className='note-item-title'>Title</div>
+            <div className='note-item-content'>Note2</div>
+          </div>
+        </div>
+        {/* <div className='note-item'>Note 3</div>
+        <div className='note-item'>Note 4</div>
+        <div className='note-item'>Note 5</div>
+        <div className='note-item'>Note 6</div>
+        <div className='note-item'>Note 7</div>
+        <div className='note-item'>Note 8</div>
+        <div className='note-item'>Note 9</div> */}
+      </div>
+    </section>
   )
 }
 export default NotesMenu
--- src/icons.jsx
+++ src/icons.jsx
@@ -328,3 +328,21 @@ export const AddTagsIcon = () => {
     </svg>
   );
 };
+
+export const AddNoteIcon = () => {
+  return (
+    <svg
+      xmlns="http://www.w3.org/2000/svg"
+      fill="none"
+      viewBox="0 0 24 24"
+      strokeWidth={1.5}
+      stroke="currentColor"
+      className="add-notes">
+      <path
+        strokeLinecap="round"
+        strokeLinejoin="round"
+        d="M12 9v6m3-3H9m12 0a9 9 0 11-18 0 9 9 0 0118 0z"
+      />
+    </svg>
+  )
+}
