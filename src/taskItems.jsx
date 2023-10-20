9308d8be7da9ac8f9b1d1aba2bbeeb4b29c6493b
Carlos Duran <krlosduran117@gmail.com>
2023-10-19 20:44:31 -0500

    Improvements to task component

--- src/taskItems.jsx
+++ src/taskItems.jsx
@@ -3,19 +3,19 @@ const taskItems = [
     id: 0,
     title: 'Make dinner',
     description: `Prepare the dinner for my Sister's birthday`,
-    subtask: {},
     dueDate: '23-12-2023',
+    listProject: '',
     tag: '',
-    project: '',
+    subtask: {},
   },
   {
     id: 1,
     title: 'Clean the house',
     description: 'Getting ready for the day',
-    subtask: {},
     dueDate: '14-02-2024',
+    listProject: '',
     tag: '',
-    project: '',
+    subtask: {},
   }
 ]
 
