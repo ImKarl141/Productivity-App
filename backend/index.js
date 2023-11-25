55a4795ec9952d77509affb0228d311694d84584
Carlos Duran <krlosduran117@gmail.com>
2023-11-25 17:49:21 -0500

    Post request created for submitting task to the database

--- backend/index.js
+++ backend/index.js
@@ -1,5 +1,7 @@
 import express from "express";
 import mysql2 from "mysql2";
+import cors from "cors";
+
 
 const app = express();
 
@@ -10,9 +12,28 @@ const db = mysql2.createConnection({
   database: "todo"
 })
 
+app.use(express.json())
+
+app.use(cors())
+
 app.get("/", (req, resp) => {
   resp.json("Hello from the backend of todo!")
-})
+});
+
+app.post("/task-current", (req, resp) => {
+  const q = "INSERT INTO task (`title`, `desc`, `taskDate`, `taskTag`) VALUES (?)";
+  const values = [
+    req.body.title,
+    req.body.desc,
+    req.body.taskDate,
+    req.body.taskTag,
+  ];
+  db.query(q, [values], (err, data) => {
+    if (err) {
+      return resp.json(err);
+    } return resp.json(data);
+  });
+});
 
 app.listen(8800, () => {
   console.log("Connected to the server!");
