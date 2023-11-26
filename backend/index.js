00935417d065f87636e0fd762c5f20f732174701
Carlos Duran <krlosduran117@gmail.com>
2023-11-26 17:49:08 -0500

    Post request created for adding new task, projects and tags

--- backend/index.js
+++ backend/index.js
@@ -16,12 +16,10 @@ app.use(express.json())
 
 app.use(cors())
 
-app.get("/", (req, resp) => {
-  resp.json("Hello from the backend of todo!")
-});
 
-app.post("/task-current", (req, resp) => {
-  const q = "INSERT INTO task (`title`, `desc`, `taskDate`, `taskTag`) VALUES (?)";
+//Create 
+app.post("/task_list", (req, resp) => {
+  const q = "INSERT INTO task_list (`title`, `desc`, `taskDate`, `taskTag`) VALUES (?)";
   const values = [
     req.body.title,
     req.body.desc,
@@ -34,6 +32,43 @@ app.post("/task-current", (req, resp) => {
     } return resp.json(data);
   });
 });
+
+app.post("/projects_list", (req, resp) => {
+  const q = "INSERT INTO projects_list (`project_name`, `project_color`) VALUES (?)";
+  const values = [
+    req.body.project_name,
+    req.body.project_color,
+  ];
+  db.query(q, [values], (err, data) => {
+    if (err) return resp.json(err);
+    return resp.json("Project added successfully")
+  })
+})
+
+app.post("/tags_list", (req, resp) => {
+  const q = "INSERT INTO tags_list (`tag_name`, `tag_color`) VALUES (?)"
+  const values = [
+    req.body.tag_name,
+    req.body.tag_color,
+  ]
+  db.query(q, [values], (err, data) => {
+    if (err) return resp.json(err);
+    return resp.json("Tag added successfully");
+  })
+})
+
+
+//Read
+app.get("/", (req, resp) => {
+  resp.json("Hello from the backend of todo!")
+});
+
+
+//Update
+
+
+//Delete
+
 
 app.listen(8800, () => {
   console.log("Connected to the server!");
