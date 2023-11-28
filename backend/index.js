8f813e4a3f7e8fd1ccd9d6d7c9ae448663de4fa5
Carlos Duran <krlosduran117@gmail.com>
2023-11-27 19:47:27 -0500

    GET requests for fetching data from components created

--- backend/index.js
+++ backend/index.js
@@ -19,11 +19,12 @@ app.use(cors())
 
 //Create 
 app.post("/task_list", (req, resp) => {
-  const q = "INSERT INTO task_list (`title`, `desc`, `taskDate`, `taskTag`) VALUES (?)";
+  const q = "INSERT INTO TaskCurrent (`task_title`, `task_desc`, `taskDate`, `task_project`, `task_tag`) VALUES (?)";
   const values = [
     req.body.title,
     req.body.desc,
     req.body.taskDate,
+    req.body.taskProject,
     req.body.taskTag,
   ];
   db.query(q, [values], (err, data) => {
@@ -34,7 +35,7 @@ app.post("/task_list", (req, resp) => {
 });
 
 app.post("/projects_list", (req, resp) => {
-  const q = "INSERT INTO projects_list (`project_name`, `project_color`) VALUES (?)";
+  const q = "INSERT INTO ProjectList (`project_name`, `project_color`) VALUES (?)";
   const values = [
     req.body.project_name,
     req.body.project_color,
@@ -46,7 +47,7 @@ app.post("/projects_list", (req, resp) => {
 })
 
 app.post("/tags_list", (req, resp) => {
-  const q = "INSERT INTO tags_list (`tag_name`, `tag_color`) VALUES (?)"
+  const q = "INSERT INTO TagList (`tag_name`, `tag_color`) VALUES (?)"
   const values = [
     req.body.tag_name,
     req.body.tag_color,
@@ -57,12 +58,42 @@ app.post("/tags_list", (req, resp) => {
   })
 })
 
+////////////////////////////////////////////////////////////////////////
 
 //Read
 app.get("/", (req, resp) => {
   resp.json("Hello from the backend of todo!")
 });
 
+app.get("/ProjectList", (req, resp) => {
+  const q = "SELECT * FROM ProjectList";
+  db.query(q, (err, data) => {
+    if (err) {
+      return resp.json(err);
+    } return resp.json(data);
+  })
+})
+
+app.get("/TagList", (req, resp) => {
+  const q = "SELECT * FROM TagList";
+  db.query(q, (err, data) => {
+    if (err) {
+      return resp.json(err);
+    } return resp.json(data);
+  })
+})
+
+app.get("/TaskCurrent", (req, resp) => {
+  // const q = "SELECT t.id, t.task_title, t.task_desc, t.task_date, p.project_name, p.project_color, t.task_tag FROM `ProjectList` p RIGHT JOIN `TaskCurrent` t ON p.id = t.task_project";
+
+  const q = "SELECT t.id, t.task_title, t.task_desc, t.task_date, p.project_name, p.project_color, tg.tag_name, tg.tag_color FROM `TaskCurrent` t LEFT JOIN `ProjectList` p ON t.task_project = p.id LEFT JOIN `TagList` tg ON t.task_tag = tg.id";
+  db.query(q, (err, data) => {
+    if (err) {
+      return resp.json(err);
+    } return resp.json(data);
+  })
+})
+////////////////////////////////////////////////////////////////////////
 
 //Update
 
