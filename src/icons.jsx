44dc405bc3f702007aaee0f9025b3645ddb299d3
Carlos Duran <krlosduran117@gmail.com>
2024-01-01 03:26:16 -0500

    Responsive design improved

--- src/icons.jsx
+++ src/icons.jsx
@@ -6,7 +6,7 @@ export const MenuIcon = () => {
       viewBox="0 0 24 24"
       strokeWidth={1.5}
       stroke="currentColor"
-      className="w-6 h-6"
+      className="menu-icon"
     >
       <path
         strokeLinecap="round"
