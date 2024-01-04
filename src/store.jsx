fd7f4eec946fdf6cd17f8707721befdf88cb5871
Carlos Duran <krlosduran117@gmail.com>
2024-01-04 18:30:11 -0500

    store file improved

--- src/store.jsx
+++ src/store.jsx
@@ -1,8 +1,8 @@
-import { configureStore } from "@reduxjs/toolkit";
+import { configureStore } from '@reduxjs/toolkit';
 import menuReducer from './features/menuSlice';
 import taskReducer from './features/taskSlice';
 import optionReducer from './features/optionSlice';
-import noteReducer from "./features/NoteSlice";
+import noteReducer from './features/NoteSlice';
 import timerReducer from './features/timerSlice';
 
 
